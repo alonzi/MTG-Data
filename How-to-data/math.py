a78d5ce93cd7e1467a775cec2869ae49dc0ad2c6
pete <lpa2a@virginia.edu>
2021-10-25 22:25:03 -0400

    add some python

---
+++ How-to-data/math.py
@@ -0,0 +1,13 @@
+# github.com/alonzi
+# a script to do the math in lesson 1
+# 2021-10-25
+
+
+import numpy as np
+
+lunv = np.array([60.0,57.7,55.0,57.7]) # a vector representing the lunarch veteran GP WR
+cplv = np.array([59.1,56.7,55.3,57.6]) # a vector representing the color pair winrates (WU,WB,RW,GW)
+
+flts = np.array([59.3,58.8,53.7,57.1]) # a vector representing the flip the switch GP WR
+cpfs = np.array([59.1,58.1,53.7,57.6]) # a vector representing the color pair winrates (WU,UB,UR,UG)
+
