60cddcb999feeadf030b397cc84ac151875ee599
Alonzi <pete.alonzi@gmail.com>
2021-12-03 18:16:47 -0500

    add kurtz file

--- 2021-12-01-17lands-premier-data/eda.py
+++ 2021-12-01-17lands-premier-data/eda.py
@@ -3,4 +3,14 @@
 # 2021-12-03
 # fun!
 
-import ROOT
+# import ROOT # need to bring in pyroot https://root.cern/manual/python/
+
+import pandas as pd
+
+filepath = "./2021-12-01-17lands-premier-data/"
+filename = "draft_data_public.VOW.PremierDraft.csv"
+filenamekurtz = "kurtz.csv"
+
+df = pd.read_csv(filepath+filenamekurtz)
+
+print(df.head)
